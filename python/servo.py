0f7502ee24bd2ac9a899ffad8ab17ce2cc345d88
ServAlex <servalex1@gmail.com>
2019-10-20 14:22:22 +0300

    added server checking for servo

--- python/servo.py
+++ python/servo.py
@@ -1,40 +1,39 @@
 import RPi.GPIO as GPIO
 import time
+import requests
+import time
 
 control = [5,5.5,6,6.5,7,7.5,8,8.5,9,9.5,10]
 
 servo = 22
 
 GPIO.setmode(GPIO.BOARD)
-
 GPIO.setup(servo,GPIO.OUT)
-# in servo motor,
-# 1ms pulse for 0 degree (LEFT)
-# 1.5ms pulse for 90 degree (MIDDLE)
-# 2ms pulse for 180 degree (RIGHT)
-
-# so for 50hz, one frequency is 20ms
-# duty cycle for 0 degree = (1/20)*100 = 5%
-# duty cycle for 90 degree = (1.5/20)*100 = 7.5%
-# duty cycle for 180 degree = (2/20)*100 = 10%
-
 p=GPIO.PWM(servo,50)# 50hz frequency
-
 p.start(2.5)# starting duty cycle ( it set the servo to 0 degree )
 
+url = "https://webpad.com.ua/servo"
+r = requests.post(url)
+print(r.status_code, r.reason)
+#sys.exit()
+
+values = range(11)
 
 try:
        while True:
            #read sarvar
-           for x in range(11):
-             p.ChangeDutyCycle(control[x])
-             time.sleep(0.03)
-             print x
-           
-           for x in range(9,0,-1):
-             p.ChangeDutyCycle(control[x])
-             time.sleep(0.03)
-             print x
+           if r.text == True:
+               for x in range(11):
+                 p.ChangeDutyCycle(control[x])
+                 time.sleep(0.03)
+                 print(x)
+               
+               for x in range(9,0,-1):
+                 p.ChangeDutyCycle(control[x])
+                 time.sleep(0.03)
+                 print(x)
+            else: 
+                time.sleep(0.1)
            
 except KeyboardInterrupt:
     GPIO.cleanup()
